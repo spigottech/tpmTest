dee8ed706155ab82000f15bfbcae6b1e756edc6a
Auguste Rame <Auguste.rame@gmail.com>
2019-02-24 12:12:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 function main() {
 
-  print("tpmTest is ready for action");
+  (() => {
+    
+    print("tpmTest is ready for action");
+    
+    class B {
+     
+      constructor() {
+       
+        this.a = 2;
+        
+      }
+      
+    }
+    
+    var c = new B();
+    print(c.a);
+    
+  })();
 
 }
